2941faba55c5c0e098485b253a6862c9ebbcda4a
Denis <ExuDenis@yandex.ru>
2024-06-21 15:24:48 +0300

    Проект на проверку

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -8,12 +8,27 @@
 import UIKit
 
 class ViewController: UIViewController {
+    var counter = 0
 
+    @IBOutlet weak var labelCounter: UILabel!
+    @IBOutlet weak var buttonCounter: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        buttonCounter.layer.cornerRadius
+        = 15
+        buttonCounter.layer.masksToBounds
+        = true
+        buttonCounter.backgroundColor = .green
+        labelCounter.text = "Значение счетчика: 0"
+
     }
 
-
+    @IBAction func addButtonCounter(_ sender: Any) {
+        print("Нажатие")
+        counter += 1
+        labelCounter.text = "Значение счетчика: \(counter)"
+        
+    }
+    
 }
 
